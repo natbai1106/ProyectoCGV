c172208927db21d9f0d0938e8caf5aa321d32185
natbai1106 <natbai1106@gmail.com>
2021-08-17 00:36:53 -0600

    Se crearon los frame para mostrar las imagenes y se esta dando estilo al disenio

--- PantallaPrincipal.py
+++ PantallaPrincipal.py
@@ -28,17 +28,11 @@ def elegir_imagen():
         lblInputImage.configure(image=img)
         lblInputImage.image = img
 
-        
         # Label IMAGEN DE ENTRADA
         lblInfo1 = Label(ventana, text="IMAGEN DE ENTRADA:")
-        lblInfo1.grid(column=0, row=1, padx=5, pady=5)
-        # Al momento que leemos la imagen de entrada, vaciamos
-        # la iamgen de salida y se limpia la selección de los
-        # radiobutton
-        #lblOutputImage.image = ""
-        #selected.set(0)
+        lblInfo1.grid(column=0, row=1, padx=5, pady=5)       
 
-#image = None
+image = None
 
 ventana = Tk()
 ventana.title("Principal")
@@ -46,26 +40,8 @@ ventana.title("Principal")
 #LABEL DONDE SE MOSTRARA LA IMAGEN DE ENTRADA
 lblInputImage = Label(ventana)
 lblInputImage.grid(column=0, row=2)
+
 #ventana.geometry("1366x768")
 botonCargar = Button(ventana, text="Elegir una imagen", width=25, command=elegir_imagen)
 botonCargar.grid(column=0, row=0, padx=5, pady=5)
-
-#frame1 = Frame(ventana,bg="blue")
-#frame1.grid(column=0, row=2, padx=5, pady=5)
-
-#frame2 = Frame(ventana,bg="yellow")
-#frame2.pack(expand=True, fill='both')
-#frame2.config(cursor='hand1')
-
-#redButton = Button(frame1,text="Red", fg="red")
-#greenButton = Button(frame1,text="Green", fg="green")
-#blueButton = Button(frame1,text="Blue", fg="blue")
-
-#redButton.place(relx=.05,rely=.05, relwidth=.25,relheight=.9)
-#greenButton.place(relx=.35,rely=.05, relwidth=.25,relheight=.9)
-#blueButton.place(relx=.65,rely=.05, relwidth=.25,relheight=.9)
-
-#blackButton = Button(frame2, text="Black", fg="black")
-#blackButton.pack()
-
 ventana.mainloop()
